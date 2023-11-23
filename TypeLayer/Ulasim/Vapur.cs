20c5dbf54ea2c2c971e59320372fbb513f6e8e21
birkanturan10 <117521602+birkanturan10@users.noreply.github.com>
2023-11-23 17:18:24 +0300

    Vapur İskeleleri API tamamlandı.

---
+++ TypeLayer/Ulasim/Vapur.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeLayer.Ulasim
+{
+	public class Vapur
+	{
+		[DisplayName("ID")]
+        public int IskeleId { get; set; }
+
+		[DisplayName("İskele Adı")]
+		public string Adi { get; set; }
+
+		[DisplayName("Aktif Mi?")]
+		public bool AktifMi { get; set; }
+
+		[DisplayName("Arabalı Vapur İskelesi Mi?")]
+		public bool ArabaliVapurIskelesiMi { get; set; }
+	}
+}
--- UI/Controllers/HomeController.cs
+++ UI/Controllers/HomeController.cs
@@ -1,12 +1,54 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net;
+using TypeLayer.Ulasim;
 
 namespace UI.Controllers
 {
-    public class HomeController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Vapurİskeleleri()
+		{
+            string json = new WebClient().DownloadString("https://openapi.izmir.bel.tr/api/izdeniz/iskeleler");
+            var vapur = JsonConvert.DeserializeObject<List<Vapur>>(json);
+            return View(vapur);
         }
-    }
+
+		//	public async Task<IActionResult> TaksiDuraklari(int page = 1, int pageSize = 10)
+		//	{
+		//		using (HttpClient client = new HttpClient())
+		//		{
+		//			string apiUrl = "https://openapi.izmir.bel.tr/api/ibb/cbs/taksiduraklari";
+		//			HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+		//			if (response.IsSuccessStatusCode)
+		//			{
+		//				string content = await response.Content.ReadAsStringAsync();
+		//				var apiResponse = JsonConvert.DeserializeObject<Taksi>(content);
+
+		//				// Veri setini sayfalamak için LINQ kullanabilirsiniz
+		//				var paginatedData = apiResponse.onemliyer
+		//					.Skip((page - 1) * pageSize)
+		//					.Take(pageSize)
+		//					.ToList();
+
+		//				// Sayfalama bilgilerini view'a ekleyin
+		//				ViewBag.PageNumber = page;
+		//				ViewBag.TotalPages = (int)Math.Ceiling((double)apiResponse.onemliyer.Count / pageSize);
+
+		//				return View(paginatedData);
+		//			}
+		//			else
+		//			{
+		//				return View("Error");
+		//			}
+		//		}
+		//	}
+		//
+	}
 }
